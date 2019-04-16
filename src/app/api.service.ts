927acc3f9be8ebb454b5fa20643c25b8f24b43ab
Sushi Shinde <sushilbshinde@gmail.com>
2019-04-16 11:39:23 +0530

    show questions component

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,6 +6,10 @@ export class ApiService {
     
     constructor(private http:HttpClient){}
 
+    getQuestions() {
+        return this.http.get('http://10.211.55.3:50426/api/questions');
+    }
+
     postQuestion(question) {
         this.http.post('http://10.211.55.3:50426/api/questions', question).subscribe(res => {
             console.log(res);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule, MatInputModule, MatCardModule} from '@angular/material';
+import { MatButtonModule, MatInputModule, MatCardModule, MatListModule } from '@angular/material';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 import { ApiService } from './api.service';
+import { QuestionsComponent } from './questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent, QuestionComponent
+    AppComponent, QuestionComponent, QuestionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
-    MatInputModule, 
-    MatCardModule
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
