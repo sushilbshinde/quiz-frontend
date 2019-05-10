6ba41bf5f3828679f1b57b0cf13e3beec6bbf7b9
Sushi Shinde <sushilbshinde@gmail.com>
2019-05-10 11:31:23 +0530

    registor form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,12 +20,13 @@ import { HomeComponent } from './home.component';
 import { NavComponent } from './nav.component';
 import { QuizComponent } from './quiz.component';
 import { QuizzesComponent } from './quizzes.component';
+import { RegisterComponent } from './register.component';
 
 const routes = [
   { path: '', component: HomeComponent },
   { path: 'question', component: QuestionComponent },
   { path: 'question/:quizId', component: QuestionComponent },
-  { path: 'questions', component: QuestionsComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'quiz', component: QuizComponent }
 ] 
 
@@ -37,7 +38,8 @@ const routes = [
     HomeComponent, 
     NavComponent, 
     QuizComponent,
-    QuizzesComponent
+    QuizzesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
