49d658de8b9555da1cadc9e6e61674a620e6698f
Sushi Shinde <sushilbshinde@gmail.com>
2019-06-06 19:31:22 +0530

    Auth token middleware

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -11,7 +11,7 @@ export class AuthInterceptor implements HttpInterceptor {
         var token = localStorage.getItem('token');
 
         var authRequest = req.clone({
-            headers: req.headers.set('Autherization', `Bearer ${token}`)
+            headers: req.headers.set('Authorization', `Bearer ${token}`)
         });
 
         return next.handle(authRequest);
