789e1143ada0c141ecd194fcd0bf6e8a0c3c846c
Sushi Shinde <sushilbshinde@gmail.com>
2019-06-06 17:08:21 +0530

    Pass token with angular requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { 
   MatButtonModule, 
   MatInputModule, 
@@ -22,6 +22,7 @@ import { QuizComponent } from './quiz.component';
 import { QuizzesComponent } from './quizzes.component';
 import { RegisterComponent } from './register.component';
 import { AuthService} from './auth.service';
+import { AuthInterceptor} from './auth.interceptor';
 
 const routes = [
   { path: '', component: HomeComponent },
@@ -56,7 +57,11 @@ const routes = [
     MatListModule,
     MatToolbarModule
   ],
-  providers: [ApiService, AuthService],
+  providers: [ApiService, AuthService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
